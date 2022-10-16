fe56bacecac4fec4434d230b8e005a85dcc51a8a
夕桐 アヤノ <111672613+AyanoYuugiri@users.noreply.github.com>
2022-10-17 01:19:26 +0800

    BRUH

--- gradle-plugin/preview-rpc-utils.patch.kt
+++ gradle-plugin/preview-rpc-utils.patch.kt
@@ -2,8 +2,8 @@
 internal fun ByteArrayOutputStream.toString(charset: java.nio.Charset) = 
 	this.toString(charset.name())
 
-internal fun java.net.URLEncoder.encode(str: String, charset: java.nio.Charset) = 
+internal fun java.net.URLEncoder.encode(str: String, charset: java.nio.charset.Charset) = 
 	java.net.URLEncoder.encode(str, charset.name())
 
-internal fun java.net.URLDecoder.decode(str: String, charset: java.nio.Charset) = 
+internal fun java.net.URLDecoder.decode(str: String, charset: java.nio.charset.Charset) = 
         java.net.URLDecoder.decode(str, charset.name())
