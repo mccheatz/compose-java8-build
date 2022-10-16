3aaa47c457b452f69bcf06d7a03dcfb46dc6de63
AyanoYuugiri <AyanoYuugiri@users.noreply.github.com>
2022-10-16 22:40:05 +0000

    fix up

--- gradle-plugin/preview-rpc-utils.patch.kt
+++ gradle-plugin/preview-rpc-utils.patch.kt
@@ -2,8 +2,3 @@
 internal fun ByteArrayOutputStream.toString(charset: java.nio.charset.Charset) = 
 	this.toString(charset.name())
 
-internal fun java.net.URLEncoder.encode(str: String, charset: java.nio.charset.Charset) = 
-	java.net.URLEncoder.encode(str, charset.name())
-
-internal fun java.net.URLDecoder.decode(str: String, charset: java.nio.charset.Charset) = 
-        java.net.URLDecoder.decode(str, charset.name())
